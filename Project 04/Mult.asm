2c0f72fe3281a370bc952b2492ecbc580e738724
ec2604 <erachoshen@gmail.com>
2016-03-17 10:43:09 +0200

    Comments and README

--- Project 04/Mult.asm
+++ Project 04/Mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M=0
+    @R1
+    D=M
+    @i
+    M=D
+(LOOP)
+    @i
+    D=M
+    @END
+    D;JEQ
+    @R2
+    D=M
+    @R0
+    D=D+M
+    @R2
+    M=D
+    @i
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+
