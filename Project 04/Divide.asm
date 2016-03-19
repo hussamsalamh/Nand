56e7a0a3332c5c85d052732ce3be75a6da17b96b
ec2604 <erachoshen@gmail.com>
2016-03-19 11:50:04 +0200

    Comments and README

--- Project 04/Divide.asm
+++ Project 04/Divide.asm
@@ -3,25 +3,67 @@
 //The remainder should be discarded.
 //You may assume both numbers are positive.
 //The program should have a running time logarithmic with respect to the input.
-
-    @R15
-    M=0
-(LOOP)
+// Mask for adding 1 to quotient in relevant bit
+@mask
+M=1
+@R15
+M=0
+// Align left-most bit of divisor with left-most bit of dividend
+(ALIGNLOOP)
     @R13
     D=M
-    D=D>>
-    @R13
-    M=D
-    @R15
-    M=M+1
-    @R13
-    D=M
-    @2
-    D=D-A
     @R14
-    M=D
-    @END
+    D=D-M
+    @DIVISION
     D;JLE
-    @LOOP
+    @R14
+    M=M<<
+    @mask
+    M=M<<
+    @ALIGNLOOP
     0;JMP
-(END)
+// Do actual division after alignment of bits
+(DIVISION)
+// If Dividend >= divisor do necessary arithmetic
+    @R13
+    D=M
+    @R14
+    D=D-M
+    @ARITHMETIC
+    D;JGE
+    @BITSHIFT
+    0;JMP
+// Subtract divisor from dividend, put 1 in relevant bit in result (from mask)
+(ARITHMETIC)
+// Set value of dividend
+    @R13
+    D=M
+    @R14
+    D=D-M
+    @R13
+    M=D
+// Set value of quotient
+    @R15
+    D=M
+    @mask
+    D=D+M
+    @R15
+    M=D
+    @BITSHIFT
+    0;JMP
+// Realignment of bits
+(BITSHIFT)
+    @R14
+    M=M>>
+    @mask
+    M=M>>
+    @CHECKMASK
+    0;JMP
+(CHECKMASK)
+    @mask
+    D=M
+    @DIVISION
+    D;JNE
+
+
+
