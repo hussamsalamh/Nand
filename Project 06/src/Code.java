3c0385d70ea887562875756f9af98ec98627f2e8
ec2604 <erachoshen@gmail.com>
2016-04-02 11:23:02 +0300

    Updated the code module

--- Project 06/src/Code.java
+++ Project 06/src/Code.java
@@ -3,25 +3,86 @@
  */
 import java.util.Hashtable;
 public  class Code {
-    static final Hashtable<String, Integer> comp0Table = new Hashtable<String,Integer>() {{
+
+    /**
+     * Static hashtable to translate comp mnemonics to binary
+     */
+    static final Hashtable<String, Integer> compTable = new Hashtable<String,Integer>() {{
         // Put predefined binary translations here
-        put("foo", 0b10);
-        put("x", 0b10);
+        // a = 0
+        put("0", 0b0101010);
+        put("1", 0b0111111);
+        put("-1", 0b0111010);
+        put("D", 0b0001100);
+        put("A", 0b0110000);
+        put("!D", 0b0001101);
+        put("!A", 0b0110001);
+        put("-D", 0b0001111);
+        put("-A", 0b0110011);
+        put("D+1", 0b0011111);
+        put("A+1", 0b0110111);
+        put("D-1", 0b0001110);
+        put("A-1", 0b0110010);
+        put("D+A", 0b0000010);
+        put("D-A", 0b0010011);
+        put("A-D", 0b0000111);
+        put("D&A", 0b0000000);
+        put("D|A", 0b0010101);
+
+        // a=1
+        put("M", 0b1110000);
+        put("!M", 0b1110001);
+        put("-M", 0b1110011);
+        put("M+1", 0b1110111);
+        put("M-1", 0b1110010);
+        put("D+M", 0b1000010);
+        put("D-M", 0b1010011);
+        put("M-D", 0b1000111);
+        put("D&M", 0b1000000);
+        put("D|M", 0b1010101);
+
+
+
+
+
+
+
+
+
     }};
-    static final Hashtable<String, Integer> comp1Table = new Hashtable<String, Integer>() {{
-        // Put predefined binary translations here
-        put("foo", 0b10);
-        put("x", 0b10);
-    }};
+
+    /**
+     * Static hashtable to translate dest mnemonics to binary
+     */
     static final Hashtable<String, Integer> destTable = new Hashtable<String, Integer>() {{
         // Put predefined binary translations here
+        // The "NULL" needs to be added when nothing appears
+        put("NULL", 0b000)
         put("M", 0b001);
         put("D", 0b010);
+        put("MD", 0b011);
+        put("A", 0b100);
+        put("AM", 0b101);
+        put("AD", 0b110);
+        put("AMD", 0b111);
     }};
+
+    /**
+     * Static hashtable to translate jmp mnemonics to binary
+     */
     static final Hashtable<String, Integer> jmpTable = new Hashtable<String, Integer>() {{
         // Put predefined binary translations here
-        put("foo", 0b10);
-        put("x", 0b10);
+
+        // The "NULL" needs to be added when nothing appears
+        put("NULL", 0b000);
+        put("JGT", 0b001);
+        put("JEQ", 0b010);
+        put("JGE", 0b011);
+        put("JLT", 0b100);
+        put("JNE", 0b101);
+        put("JLE", 0b110);
+        put("JMP", 0b111);
+
     }};
 
     /**
@@ -30,7 +91,7 @@ public  class Code {
      * @return - The relevant 3 bits after translation.
      */
     public static int dest(String destMem){
-        return 0;
+        return destTable.get(destMem);
     }
 
     /**
