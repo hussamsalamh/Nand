0c8ea94d3cfe2fcf1716cbd6ae0080a3f278ca31
ec2604 <erachoshen@gmail.com>
2016-05-31 20:10:42 +0300

    sd

---
+++ Project 11/src/JackAnalyzer.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+//just so I get it
+/**
+ * Created by yonilip on 5/23/16.
+ *
+ *
+ * top-level driver that sets up and invokes the other modules;
+ *
+ * The analyzer program operates on a given source, where source is either a file name
+ * of the form Xxx.jack or a directory name containing one or more such files. For
+ * each source Xxx.jack file, the analyzer goes through the following logic:
+ * 1. Create a JackTokenizer from the Xxx.jack input file.
+ * 2. Create an output file called Xxx.xml and prepare it for writing.
+ * 3. Use the CompilationEngine to compile the input JackTokenizer into the output file.
+ *
+ *
+ *
+ * Notes from submission: TODO make sure this is accounted for
+ * The ' " ' sign is never translated.
+ * The command is: JackAnalyzer source. Where source is either an .jack file or a directory where the *.jack file are.
+ * All .xml files should be inside this (where the .jack files are) directory.
+ * A code is legal only if after removing the comment and replacing it with one white space it is a legal code.
+ * The submitted project should not create the T.xml files.
+ * We will use diff -w to compare your files.
+ * Escaped characters should not be interpret. '\t' is translated to '\t'.
+ * Empty tags should be translated: "<xxx> \n </xxx>" where \n is a new line.
+ * You can assume the files are a legal jack files ( you do not need to handle illegal files).
+ */
+public class JackAnalyzer {
+
+
+    private static String[] getFileArray(String pathName)
+    {
+        File directory = new File(pathName);
+        List<String> fileArray = new ArrayList<String>();
+        if (directory.isDirectory())
+        {
+            // If the path is a directory, the name of the file to write to is the name of the file in dir.
+            for (File file : directory.listFiles())
+            {
+                // append to array all the files that end with jack
+                String fileName = file.getName();
+                int indexOfSuffix = fileName.lastIndexOf(".");
+                if (indexOfSuffix > 0) {
+                    String suffix = fileName.substring(indexOfSuffix);
+                    if (suffix.equals(".jack")) {
+                        fileArray.add(file.getAbsolutePath());
+                    }
+                }
+            }
+        }
+        else
+        {
+            int indexOfSuffix = directory.getAbsolutePath().lastIndexOf(".");
+            if (indexOfSuffix == -1)
+            {
+                indexOfSuffix = 0;
+            }
+            // return array with single file, check that has jack
+            if (directory.getAbsolutePath().substring(indexOfSuffix).equals(".jack")) {
+                fileArray.add(directory.getAbsolutePath());
+            }
+        }
+        return fileArray.toArray(new String[fileArray.size()]);
+    }
+
+    /**
+     * Replace the suffix of given input with given newSuffix
+     * @param inputName the name to make output from
+     * @param newSuffix the suffix that should be output (should include dot in beginning)
+     * @return the output with new suffix, if suffix exists
+     */
+    private static String replaceSuffix(String inputName, String newSuffix) {
+        String outputName = inputName;
+        int indexOfSuffix = outputName.lastIndexOf('.');
+        if (indexOfSuffix > 0) {
+            outputName = outputName.substring(0, indexOfSuffix) + newSuffix;
+        }
+        return outputName;
+    }
+
+
+    public static void main(String[] args) {
+        String[] fileNameArray = JackAnalyzer.getFileArray(args[0]);
+        System.out.println(fileNameArray.length);
+        for (int i = 0; i < fileNameArray.length; i++)
+        {
+            String outputName = replaceSuffix(fileNameArray[i], ".xml"); //TODO change this in pr11
+            try(FileReader fr = new FileReader(fileNameArray[i]);BufferedReader br = new BufferedReader(fr);
+                FileWriter fw = new FileWriter(outputName); BufferedWriter bw = new BufferedWriter(fw))
+            {
+                CompilationEngine ce = new CompilationEngine(br, bw); //should create JackTokenizer
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ Project 11/src/JackTokenizer.java
@@ -0,0 +1,250 @@
+import java.io.*;
+import java.util.HashSet;
+// era fix for git
+/**
+ * Created by yonilip on 5/23/16.
+ *
+ *
+ * Removes all comments and white space from the input stream and
+ * breaks it into Jack-language tokens, as specified by the Jack grammar
+ */
+public class JackTokenizer {
+
+
+    public enum LexicalElements {KEYWORD, SYMBOL, IDENTIFIER, integerConstant, stringConstant}
+
+    public enum Keyword {CLASS, METHOD, FUNCTION,CONSTRUCTOR, INT, BOOLEAN, CHAR, VOID, VAR, STATIC, FIELD, LET,
+        DO ,IF, ELSE, WHILE, RETURN, TRUE, FALSE, NULL, THIS}
+
+    /*private static final HashMap<String, Keyword> keywordsSet = new HashMap<String, Keyword>() {{
+        put("class", Keyword.CLASS);
+        put("constructor", Keyword.CONSTRUCTOR);
+        put("function", Keyword.FUNCTION);
+        put("method", Keyword.METHOD);
+        put("field", Keyword.FIELD);
+        put("static", Keyword.STATIC);
+        put("var", Keyword.VAR);
+        put("int", Keyword.INT);
+        put("char", Keyword.CHAR);
+        put("boolean", Keyword.BOOLEAN);
+        put("void", Keyword.VOID);
+        put("true", Keyword.TRUE);
+        put("false", Keyword.FALSE);
+        put("null", Keyword.NULL);
+        put("this", Keyword.THIS);
+        put("let", Keyword.LET);
+        put("do", Keyword.DO);
+        put("if", Keyword.IF);
+        put("else", Keyword.ELSE);
+        put("while", Keyword.WHILE);
+        put("return", Keyword.RETURN);
+    }};
+    */
+    private static final HashSet<String> keywordsSet = new HashSet<String>() {{
+        add("class");
+        add("constructor");
+        add("function");
+        add("method");
+        add("field");
+        add("static");
+        add("var");
+        add("int");
+        add("char");
+        add("boolean");
+        add("void");
+        add("true");
+        add("false");
+        add("null");
+        add("this");
+        add("let");
+        add("do");
+        add("if");
+        add("else");
+        add("while");
+        add("return");
+    }};
+
+
+    protected static final HashSet<Character> symbolSet = new HashSet<Character>() {{
+        add('{');
+        add('}');
+        add('(');
+        add(')');
+        add('[');
+        add(']');
+        add('.');
+        add(',');
+        add(';');
+        add('+');
+        add('-');
+        add('*');
+        add('/');
+        add('&');
+        add('|');
+        add('<');
+        add('>');
+        add('=');
+        add('~');
+    }};
+
+
+    private StreamTokenizer streamTokenizer;
+    private int currentToken;
+    private LexicalElements curTokenType;
+    private String stringValue;
+
+
+    /**
+     * Opens the input file/stream and gets
+     * ready to tokenize it.
+     * @param file
+     */
+    public JackTokenizer(BufferedReader file) {
+        streamTokenizer = new StreamTokenizer(file);
+        streamTokenizer.quoteChar('\"'); //TODO make sure that dont need to catch single quote
+        streamTokenizer.ordinaryChar('-');
+        streamTokenizer.ordinaryChar('.');
+        streamTokenizer.ordinaryChar('/');
+        streamTokenizer.wordChars('_', '_');
+        streamTokenizer.ordinaryChar(':');
+        streamTokenizer.wordChars(':', ':');
+        streamTokenizer.slashSlashComments(true);
+        streamTokenizer.slashStarComments(true);
+    }
+
+
+
+    /**
+     * Do we have more tokens in the input?
+     * @return
+     */
+    public boolean advance() throws IOException {
+        currentToken = streamTokenizer.nextToken();
+        return currentToken != StreamTokenizer.TT_EOF;
+    }
+
+    /**
+     * Returns the type of the current token
+     * @return
+     */
+    public LexicalElements tokenType() throws IOException {
+        if (currentToken == StreamTokenizer.TT_WORD) {
+            // use streamTokenizer.sval
+            //if (keywordsSet.containsKey(streamTokenizer.sval.toLowerCase())) {
+            if (keywordsSet.contains(streamTokenizer.sval.toLowerCase())) {
+                return LexicalElements.KEYWORD;
+            }
+            else return LexicalElements.IDENTIFIER;
+        }
+        else if (currentToken == StreamTokenizer.TT_NUMBER) {
+            return LexicalElements.integerConstant;
+        }
+        else if (symbolSet.contains((char)currentToken)) {
+            return LexicalElements.SYMBOL;
+        }
+        else if (currentToken == '\"') {
+            //String originalString = streamTokenizer.toString();
+            stringValue = streamTokenizer.toString();
+            int findEnd = 0;
+            while (stringValue.indexOf("]", findEnd+1) != -1)
+            {
+                findEnd = stringValue.indexOf("]", findEnd+1);
+            }
+            stringValue = stringValue.substring(6, findEnd);
+            //advance();
+            //   while (currentToken != '\"') {
+            //       advance();
+            //       stringValue += (char)currentToken;
+            //   }
+            stringValue = stringValue.replace("\t", "\\t");
+            stringValue = stringValue.replace("&", "&amp;");
+            stringValue = stringValue.replace("<", "&lt;");
+            stringValue = stringValue.replace(">", "&gt;");
+            stringValue = stringValue.replace("\"", "&quot;");
+            stringValue = stringValue.replace("\n", "\\n");
+
+            return LexicalElements.stringConstant;
+        }
+        return null; // case shouldnt happen
+    }
+
+    /**
+     * Returns the keyword which is the current token. Should be called only when tokenType() is KEYWORD .
+     * @return
+     */
+    public String keyWord() {
+        //return keywordsSet.get(streamTokenizer.sval.toLowerCase());
+        return streamTokenizer.sval.toLowerCase();
+    }
+
+    /**
+     * Returns the character which is the current token. Should be called only when tokenType() is SYMBOL .
+     * @return
+     */
+    public char symbol() {
+        return (char) currentToken;
+    }
+
+    /**
+     * Returns the identifier which is the current token. Should be called only when tokenType() is IDENTIFIER .
+     * @return
+     */
+    public String identifier() {
+        return streamTokenizer.sval;
+    }
+
+    /**
+     * Returns the integer value of the current token. Should be called only when tokenType() is integerConstant .
+     * @return
+     */
+    public int intVal() {
+        return (int) streamTokenizer.nval;
+    }
+
+    /**
+     * Returns the string value of the current token, without the double quotes. Should be called only when
+     * tokenType() is stringConstant .
+     * @return
+     */
+    public String stringVal() {
+        return stringValue;
+    }
+
+    /**
+     * Test!
+     * TODO dont submit this!
+     * @param args
+     */
+    public static void main(String[] args) {
+        String str = args[0];
+        try(FileReader fr1 = new FileReader(str); BufferedReader r = new BufferedReader(fr1)) {
+            JackTokenizer a = new JackTokenizer(r);
+            while (a.advance()) {
+                switch (a.tokenType()) {
+                    case KEYWORD:
+                        System.out.println("KEYWORD: " + a.keyWord());
+                        break;
+                    case SYMBOL:
+                        System.out.println("SYMBOL: " + a.symbol());
+                        break;
+                    case IDENTIFIER:
+                        System.out.println("IDENT: " + a.identifier());
+                        break;
+                    case integerConstant:
+                        System.out.println("INT: " + a.intVal() );
+                        break;
+                    case stringConstant:
+                        System.out.println("STRING: " + a.stringVal());
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
